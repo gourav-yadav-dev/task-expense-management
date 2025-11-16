916943b9974d2b40b019d763df4ca6168ecd8485
gourav-yadav-dev <yadavgourav263@gmail.com>
2025-11-16 16:08:34 +0530

    Write logic for profile

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export function Header() {
+    const [userToken, setUserToken] = useState('')
+    const [alreadyLogin, setAlreadyLogin] = useState()
+    useEffect(() => {
+        let useToken = localStorage.getItem('token')
+        if (useToken === null) {
+            setUserToken("Profile")
+            setAlreadyLogin(false)
+        }
+        else {
+            let useName = localStorage.getItem('name')
+            setUserToken(useName)
+            setAlreadyLogin(true)
+        }
+
+    }, [])
+
+
+
     return (
         <header className="d-flex justify-content-between align-items-center p-3 shadow-sm bg-white">
             <div className="fw-bold fs-4">
@@ -13,12 +33,15 @@ export function Header() {
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
                 >
-                    Profile
+                    {userToken}
                 </button>
 
                 <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                    <li><a className="dropdown-item" href="#">My Profile</a></li>
-                    <li><a className="dropdown-item" href="#">Logout</a></li>
+                    {alreadyLogin ? <div><li><button className="dropdown-item" href="#">Logout</button></li>
+                    </div>
+                        : <div><li><Link className="dropdown-item" >Signup</Link></li>
+                            <li><Link className="dropdown-item" >Login</Link></li></div>}
+
                 </ul>
             </div>
         </header>
