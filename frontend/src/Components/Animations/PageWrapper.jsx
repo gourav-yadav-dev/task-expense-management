dc15b8835db2034f62a28609d9bfa6998697e019
gourav-yadav-dev <yadavgourav263@gmail.com>
2025-11-17 22:33:43 +0530

    Add animation PageWrapper.jsx

---
+++ frontend/src/Components/Animations/PageWrapper.jsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion";
+
+export function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.4 }}
+      className="w-full"
+    >
+      {children}
+    </motion.div>
+  );
+}
